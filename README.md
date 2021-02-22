cdb6c7b78a80bc3f219f902dd022de60eec6a922
Tim Kozak <tima.kozak@gmail.com>
2021-02-22 21:01:44 +0200

    redo find_by_keys

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # JsonNavigator
+
 Navigate through Twitter json files
+
+Program is launched from json_navigator.py file and you need to have a twitter api token to proceed.
--- json_navigator.py
+++ json_navigator.py
@@ -10,10 +10,11 @@ def twitter_api():
     """
     Get json from twitter
     """
+
     tweet_tag = input("Enter twitter tag: @")
     base_url = "https://api.twitter.com/"
 
-    bearer_token = ""
+    bearer_token = "AAAAAAAAAAAAAAAAAAAAAIKvMwEAAAAAGfXtAizYuenmkYuQLZ4qCDL%2F3n8%3D0Bqy4HA6TVWqzcYq7Nk9UK0slRiRyVZvlEBtpyowkdXOupLBNf"
 
     search_url = '{}1.1/friends/list.json'.format(base_url)
 
@@ -36,33 +37,65 @@ def find_by_keys(twitter_dict: str):
     Extract values from json by keys.
 
     """
-    num = int(input("Enter a random number and recieve info about one friend: "))
-    twitter = twitter_dict[num]
+    twitter = twitter_dict
     while True:
-        if isinstance(twitter, dict):
 
-            print("---"*10)
-            pprint(list(twitter.keys()))
-            print("---"*10)
-            while True:
+        if isinstance(twitter, dict):
+            try:
                 key = input("Pick an existing key and recieve info: ")
                 if key in twitter.keys():
                     print("---"*10)
                     pprint(twitter[key])
                     print("---"*10)
-                    if isinstance(twitter[key], dict):
-                        print("---"*10)
-                        pprint(list(twitter[key]))
-                        print("---"*10)
-                        twitter = twitter[key]
-                    else:
-                        return twitter[key]
+
+                if isinstance(twitter[key], dict):
+                    print("---"*10)
+                    pprint(list(twitter[key]))
+                    print("---"*10)
+                    twitter = twitter[key]
+
+                elif isinstance(twitter[key], list):
+                    print("---"*10)
+                    pprint(twitter[key])
+                    print("---"*10)
+                    twitter = twitter[key]
 
                 else:
-                    print("Wrong key!")
-                    endpoint = input("Do you want to end? (y/n) ")
-                    if endpoint == "y":
-                        break
+                    return twitter[key]
+            except KeyError:
+                print("Invalid key")
+                endpoint = input("Do you want to end? (y/n) ")
+                if endpoint == "y":
+                    break
+
+        elif isinstance(twitter, list):
+            try:
+                index = int(
+                    input("Pick an existing index and recieve info: "))
+                if index in range(len(twitter)):
+                    print("---"*10)
+                    pprint(twitter[index])
+                    print("---"*10)
+
+                if isinstance(twitter[index], dict):
+                    print("---"*10)
+                    pprint(list(twitter[index]))
+                    print("---"*10)
+                    twitter = twitter[index]
+
+                elif isinstance(twitter[index], list):
+                    print("---"*10)
+                    pprint(twitter[index])
+                    print("---"*10)
+                    twitter = twitter[index]
+
+                else:
+                    return twitter[index]
+            except IndexError:
+                print("Invalid index")
+                endpoint = input("Do you want to end? (y/n) ")
+                if endpoint == "y":
+                    break
 
 
 if __name__ == "__main__":
